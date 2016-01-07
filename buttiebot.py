ba842646a3d884ca08db76bc2e1b222dce1dc7c9
Filipe Pina <fopina@gmail.com>
2016-01-07 19:05:18 +0000

    cleanup

--- buttiebot.py
+++ buttiebot.py
@@ -78,10 +78,8 @@ def build_parser():
     parser.add_argument('--create_db', dest='create_db', action='store_const',
                         const=True, default=False,
                         help='setup database')
-
     parser.add_argument('--token', '-t', dest='token',
-                        help='use webhooks (instead of polling) - requires bottle')
-
+                        help='token provided by @BotFather')
     parser.add_argument('--cron', dest='cron', nargs=2, metavar=('action', 'param'),
                         help='trigger cron ACTION with PARAM parameter and quit')
 
